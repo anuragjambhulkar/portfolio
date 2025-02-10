c2b9bcc96989ddffc506c8c16e2800310fbdd904
Anurag Jambhulkar <jambhulkaranurag12@gmail.com>
2025-02-10 22:36:14 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -10,10 +10,10 @@ export const gridItems = [
     id: 1,
     title: "I prioritize client collaboration, fostering open communication ",
     description: "",
-    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]  w-full ",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh] w-full",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/portfolio/b1.svg",
+    img: "/portfolio/b1.avif",
     spareImg: "",
   },
   {
@@ -40,22 +40,21 @@ export const gridItems = [
     id: 4,
     title: "Tech enthusiast with a passion for development.",
     description: "",
-    className: "lg:col-span-3 md:col-span-3 md:row-span-1 items-top ",
+    className: "lg:col-span-3 md:col-span-3 md:row-span-1 items-top",
     imgClassName: "",
-    titleClassName: "lg:justify-center md:justify-start -top-5 ",
-    img: "/portfolio/grid.svg",
-    spareImg: "/portfolio/b4.svg",
+    titleClassName: "lg:justify-center md:justify-start -top-5",
+    img: "/portfolio/grid.avif",
+    spareImg: "/portfolio/b4.avif",
   },
-
   {
     id: 5,
     title: "Currently building a JS Animation library",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-3",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
-    titleClassName: "justify-center  lg:justify-center",
-    img: "/portfolio/b5.svg",
-    spareImg: "/portfolio/grid.svg",
+    titleClassName: "justify-center lg:justify-center",
+    img: "/portfolio/b5.avif",
+    spareImg: "/portfolio/grid.avif",
   },
   {
     id: 6,
@@ -74,66 +73,33 @@ export const projects = [
     id: 1,
     title: "3D Solar System Planets to Explore",
     des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/portfolio/p1.svg",
-    iconLists: ["/portfolio/re.svg", "/portfolio/tail.svg", "/portfolio/ts.svg", "/portfolio/three.svg", "/portfolio/fm.svg"],
+    img: "/portfolio/p1.avif",
+    iconLists: ["/portfolio/re.avif", "/portfolio/tail.avif", "/portfolio/ts.avif", "/portfolio/three.avif", "/portfolio/fm.avif"],
     link: "https://github.com/adrianhajdin?tab=repositories",
   },
   {
     id: 2,
     title: "Yoom - Video Conferencing App",
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/portfolio/p2.svg",
-    iconLists: ["/portfolio/next.svg", "/portfolio/tail.svg", "/portfolio/ts.svg", "/portfolio/stream.svg", "/portfolio/c.svg"],
+    img: "/portfolio/p2.avif",
+    iconLists: ["/portfolio/next.avif", "/portfolio/tail.avif", "/portfolio/ts.avif", "/portfolio/stream.avif", "/portfolio/c.avif"],
     link: "https://github.com/adrianhajdin/zoom-clone",
   },
   {
     id: 3,
     title: "AI Image SaaS - Canva Application",
     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/portfolio/p3.svg",
-    iconLists: ["/portfolio/re.svg", "/portfolio/tail.svg", "/portfolio/ts.svg", "/portfolio/three.svg", "/portfolio/c.svg"],
+    img: "/portfolio/p3.avif",
+    iconLists: ["/portfolio/re.avif", "/portfolio/tail.avif", "/portfolio/ts.avif", "/portfolio/three.avif", "/portfolio/c.avif"],
     link: "https://github.com/adrianhajdin/ai_saas_app",
   },
   {
     id: 4,
     title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/portfolio/p4.svg",
-    iconLists: ["/portfolio/next.svg", "/portfolio/tail.svg", "/portfolio/ts.svg", "/portfolio/three.svg", "/portfolio/gsap.svg"],
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects.",
+    img: "/portfolio/p4.avif",
+    iconLists: ["/portfolio/next.avif", "/portfolio/tail.avif", "/portfolio/ts.avif", "/portfolio/three.avif", "/portfolio/gsap.avif"],
     link: "https://github.com/adrianhajdin/iphone",
-  },
-];
-
-export const testimonials = [
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
   },
 ];
 
@@ -141,32 +107,32 @@ export const companies = [
   {
     id: 1,
     name: "cloudinary",
-    img: "/portfolio/cloud.svg",
-    nameImg: "/portfolio/cloudName.svg",
+    img: "/portfolio/cloud.avif",
+    nameImg: "/portfolio/cloudName.avif",
   },
   {
     id: 2,
     name: "appwrite",
-    img: "/portfolio/app.svg",
-    nameImg: "/portfolio/appName.svg",
+    img: "/portfolio/app.avif",
+    nameImg: "/portfolio/appName.avif",
   },
   {
     id: 3,
     name: "HOSTINGER",
-    img: "/portfolio/host.svg",
-    nameImg: "/portfolio/hostName.svg",
+    img: "/portfolio/host.avif",
+    nameImg: "/portfolio/hostName.avif",
   },
   {
     id: 4,
     name: "stream",
-    img: "/portfolio/s.svg",
-    nameImg: "/portfolio/streamName.svg",
+    img: "/portfolio/s.avif",
+    nameImg: "/portfolio/streamName.avif",
   },
   {
     id: 5,
     name: "docker.",
-    img: "/portfolio/dock.svg",
-    nameImg: "/portfolio/dockerName.svg",
+    img: "/portfolio/dock.avif",
+    nameImg: "/portfolio/dockerName.avif",
   },
 ];
 
@@ -176,42 +142,42 @@ export const workExperience = [
     title: "Frontend Engineer Intern",
     desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
     className: "md:col-span-2",
-    thumbnail: "/portfolio/exp1.svg",
+    thumbnail: "/portfolio/exp1.avif",
   },
   {
     id: 2,
     title: "Mobile App Dev - JSM Tech",
     desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/portfolio/exp2.svg",
+    className: "md:col-span-2",
+    thumbnail: "/portfolio/exp2.avif",
   },
   {
     id: 3,
     title: "Freelance App Dev Project",
     desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/portfolio/exp3.svg",
+    className: "md:col-span-2",
+    thumbnail: "/portfolio/exp3.avif",
   },
   {
     id: 4,
     title: "Lead Frontend Developer",
     desc: "Developed and maintained user-facing features using modern frontend technologies.",
     className: "md:col-span-2",
-    thumbnail: "/portfolio/exp4.svg",
+    thumbnail: "/portfolio/exp4.avif",
   },
 ];
 
 export const socialMedia = [
   {
     id: 1,
-    img: "/portfolio/git.svg",
+    img: "/portfolio/git.avif",
   },
   {
     id: 2,
-    img: "/portfolio/twit.svg",
+    img: "/portfolio/twit.avif",
   },
   {
     id: 3,
-    img: "/portfolio/link.svg",
+    img: "/portfolio/link.avif",
   },
 ];
