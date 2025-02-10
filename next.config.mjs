4bf9269a97cb8a3bf45cd6134343421ab11d5361
Anurag Jambhulkar <jambhulkaranurag12@gmail.com>
2025-02-10 20:35:43 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-    output: 'export',
-    typescript: {
-        ignoreBuildErrors: true,
-    }
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  basePath: '/portfolio', // Add your desired base path here
 };
 
-export default nextConfig; // Correct for .mjs files
+export default nextConfig;
