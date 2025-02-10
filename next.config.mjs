0aec7e9977f344c1feeb39fb6678c22ed0321f99
Anurag Jambhulkar <jambhulkaranurag12@gmail.com>
2025-02-10 18:59:36 +0530

    Update next.config.mjs
    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:'export'
+  output: "export",
+  images: {
+    unoptimized: true, // Fixes image issues on GitHub Pages
+  }
     ,typescript:{
         ignoreBuildErrors:true ,
     }
