b77f9541ba16f78c00c9ea91fd60d16c0a52cc0d
anuragjambhulkar <jambhulkaranurag12@gmail.com>
2025-02-10 22:42:08 +0530

    init

--- data/index.ts
+++ data/index.ts
@@ -10,7 +10,7 @@ export const gridItems = [
     id: 1,
     title: "I prioritize client collaboration, fostering open communication ",
     description: "",
-    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh] w-full",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]  w-full ",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
     img: "/portfolio/b1.avif",
@@ -40,19 +40,20 @@ export const gridItems = [
     id: 4,
     title: "Tech enthusiast with a passion for development.",
     description: "",
-    className: "lg:col-span-3 md:col-span-3 md:row-span-1 items-top",
+    className: "lg:col-span-3 md:col-span-3 md:row-span-1 items-top ",
     imgClassName: "",
-    titleClassName: "lg:justify-center md:justify-start -top-5",
+    titleClassName: "lg:justify-center md:justify-start -top-5 ",
     img: "/portfolio/grid.avif",
     spareImg: "/portfolio/b4.avif",
   },
+
   {
     id: 5,
     title: "Currently building a JS Animation library",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-3",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
-    titleClassName: "justify-center lg:justify-center",
+    titleClassName: "justify-center  lg:justify-center",
     img: "/portfolio/b5.avif",
     spareImg: "/portfolio/grid.avif",
   },
@@ -96,10 +97,43 @@ export const projects = [
   {
     id: 4,
     title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects.",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
     img: "/portfolio/p4.avif",
     iconLists: ["/portfolio/next.avif", "/portfolio/tail.avif", "/portfolio/ts.avif", "/portfolio/three.avif", "/portfolio/gsap.avif"],
     link: "https://github.com/adrianhajdin/iphone",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
   },
 ];
 
@@ -148,14 +182,14 @@ export const workExperience = [
     id: 2,
     title: "Mobile App Dev - JSM Tech",
     desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-    className: "md:col-span-2",
+    className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/portfolio/exp2.avif",
   },
   {
     id: 3,
     title: "Freelance App Dev Project",
     desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2",
+    className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/portfolio/exp3.avif",
   },
   {
@@ -180,4 +214,4 @@ export const socialMedia = [
     id: 3,
     img: "/portfolio/link.avif",
   },
-];
+]; 
